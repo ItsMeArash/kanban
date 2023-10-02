533cac029cd05b7f3bef0ddfe5677f83540d7812
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2023-10-02 19:12:50 +0330

    Routes moved to seperate file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { lazy } from 'react';
-
-import Loadable from 'ui-component/Loadable';
-
 import { useSelector } from 'react-redux';
 
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
-
-// routing
-// import Routes from 'routes';
-import { Routes, Route } from 'react-router-dom';
 
 // defaultTheme
 import themes from 'themes';
@@ -17,20 +9,11 @@ import themes from 'themes';
 // project imports
 import NavigationScroll from 'layout/NavigationScroll';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import Layout from 'layout/Layout';
-import MainLayout from 'layout/MainLayout';
-import Projects from 'views/projects';
-import AboutUs from 'views/aboutus';
-import SamplePage from 'views/sample-page';
-import RequiredAuth from 'routes/RequireAuth';
-import Login from 'views/pages/authentication/authentication3/Login3';
-import Register from 'views/pages/authentication/authentication3/Register3';
+import CustomRoutes from 'routes';
 
 // ==============================|| APP ||============================== //
 const queryClient = new QueryClient();
 const App = () => {
-  const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
-
   const customization = useSelector((state) => state.customization);
 
   return (
@@ -39,22 +22,7 @@ const App = () => {
         <ThemeProvider theme={themes(customization)}>
           <CssBaseline />
           <NavigationScroll>
-            {/* <Routes /> */}
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/" element={<MainLayout />}>
-                <Route path="/" element={<DashboardDefault />} />
-                <Route path="dashboard">
-                  <Route path="default" element={<DashboardDefault />} />
-                </Route>
-                <Route path="projects" element={<Projects />} />
-                <Route path="aboutus" element={<AboutUs />} />
-                <Route element={<RequiredAuth />}>
-                  <Route path="sample-page" element={<SamplePage />} />
-                </Route>
-              </Route>
-            </Routes>
+            <CustomRoutes />
           </NavigationScroll>
         </ThemeProvider>
       </StyledEngineProvider>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,36 @@
-import { useRoutes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-// routes
-import MainRoutes from './MainRoutes';
-import AuthenticationRoutes from './AuthenticationRoutes';
+import { lazy } from 'react';
 
-// ==============================|| ROUTING RENDER ||============================== //
+import Loadable from 'ui-component/Loadable';
 
-export default function ThemeRoutes() {
-  return useRoutes([MainRoutes, AuthenticationRoutes]);
-}
+import MainLayout from '../layout/MainLayout';
+import Projects from '../views/projects';
+import AboutUs from '../views/aboutus';
+import SamplePage from '../views/sample-page';
+import RequiredAuth from '../routes/RequireAuth';
+import Login from '../views/pages/authentication/authentication3/Login3';
+import Register from '../views/pages/authentication/authentication3/Register3';
+
+const CustomRoutes = () => {
+  const DashboardDefault = Loadable(lazy(() => import('../views/dashboard/Default')));
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<DashboardDefault />} />
+        <Route path="dashboard">
+          <Route path="default" element={<DashboardDefault />} />
+        </Route>
+        <Route path="projects" element={<Projects />} />
+        <Route path="aboutus" element={<AboutUs />} />
+        <Route element={<RequiredAuth />}>
+          <Route path="sample-page" element={<SamplePage />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
+export default CustomRoutes;
